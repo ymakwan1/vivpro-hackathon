1a5407d7d6736350cfae2a97b68966c6885bb148
ymakwan1 <ymakwan1@binghamton.edu>
2026-02-07 16:57:48 -0500

    changed some rules

--- backend/services/brain.py
+++ backend/services/brain.py
@@ -11,12 +11,13 @@ class QueryBrain:
         Return ONLY a JSON object. 
         
         Rules:
-        - When a user mentions 'open' or 'active' trials, map the overall_status to RECRUITING. If they mention 'completed' or 'finished', map it to COMPLETED. Always return the status in uppercase to match the dataset. 
+        - When a user mentions 'open' or 'active' trials, map the overall_status to RECRUITING. If they mention 'completed' or 'finished', map it to COMPLETED. Always return the status in uppercase to match the dataset. Also match for other synonyms 
         - phase: Map "Phase 1" to "PHASE1", "Phase 2" to "PHASE2", etc.
         - city: Extract the city name if present.
         - condition: Extract the medical condition (e.g., Asthma, Cancer).
+        - country: Extract country names (e.g., "USA", "France").
         - sponsor: Extract the name of the organization or agency (e.g., NIH, Pfizer).
-
+        Note: "open" can also be open-ended as overall status in NOT RECRUITING, but some facilities can be recruitng in the trial.
         Example: "open phase 3 trials in Miami for Asthma"
         Output: {{"overall_status": "RECRUITING", "phase": "PHASE3", "city": "Miami", "condition": "Asthma"}}
         """
